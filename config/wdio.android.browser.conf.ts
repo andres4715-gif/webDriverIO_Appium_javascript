f96210e06c6b395c69e25f17885f8c630fdb7bdf
arios <andres.rios@ctg.com>
2023-03-31 07:26:17 -0500

    typescript working fine

--- config/wdio.android.browser.conf.ts
+++ config/wdio.android.browser.conf.ts
@@ -19,7 +19,7 @@ config.capabilities = [
         // For W3C the appium capabilities need to have an extension prefix
         // http://appium.io/docs/en/writing-running-appium/caps/
         // This is `appium:` for all Appium Capabilities which can be found here
-        'appium:deviceName': 'AutomationTest',
+        'appium:deviceName': 'Pixel 4',
         'appium:platformVersion': '11.0',
         'appium:orientation': 'PORTRAIT',
         'appium:newCommandTimeout': 240,
